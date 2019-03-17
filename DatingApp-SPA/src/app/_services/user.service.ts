9a1202ac9d4691f749a7b34cbcea27152de5c020
heroes-coding <jeremy@heroes.report>
2019-03-17 17:29:48 -0400

    Going to St. Louis

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../environments/environment";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "../_models/user";
+import { PaginatedResult } from "../_models/pagination";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -11,8 +13,27 @@ export class UserService {
   baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl + "users");
+  getUsers(page?, itemsPerPage?): Observable<PaginatedResult<User[]>> {
+    const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<
+      User[]
+    >();
+    let params = new HttpParams();
+    if (page && itemsPerPage) {
+      params = params.append("pageNumber", page);
+      params = params.append("pageSize", itemsPerPage);
+    }
+    return this.http
+      .get<User[]>(this.baseUrl + "users", { observe: "response", params })
+      .pipe(
+        map(response => {
+          paginatedResult.result = response.body;
+          const pagination = response.headers.get("Pagination");
+          if (pagination) {
+            paginatedResult.pagination = JSON.parse(pagination);
+          }
+          return paginatedResult;
+        })
+      );
   }
 
   getUser(id: number): Observable<User> {
--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 
 namespace DatingApp.API.Helpers
 {
   public static class Extensions
   {
+
+    public static void AddPagination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+    {
+      var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+      var camelCaseFormatter = new JsonSerializerSettings();
+      camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+      response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+    }
+
     public static void AddApplicationError(this HttpResponse response, string message)
     {
       response.Headers.Add("Application-Error", message);
